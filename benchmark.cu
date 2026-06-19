6d1159bfb9fccb366aacd54bf00e72db9ced2fb2
lioren52 <arulsaini15@gmail.com>
2026-06-19 10:17:00 +0530

    Multiple Size Matrix Benchmark

--- benchmark.cu
+++ benchmark.cu
@@ -3,83 +3,92 @@
 #include <iostream>
 
 int main() {
-    int row_A = 4096;
-    int N = 4096;
-    int col_B = 4096;
 
-    std::vector<float> A_h(row_A * N);
-    std::vector<float> B_h(N * col_B);
-    std::vector<float> C_h(row_A * col_B, 0.0f);
-    float cublasPer = 0.0f;
+    std::vector<int> mSize = {256, 512, 1024, 2048, 4096, 8192};
 
-    printf("Loading matrices from disk...\n");
-    loadBinaryWeights("matrix_A.bin", A_h);
-    loadBinaryWeights("matrix_B.bin", B_h);
-    printf("Matrices loaded successfully.\n");
+    for (int item : mSize) {
+        std::cout << "Benchmarking for " << item << std::endl;
+        int row_A = item;
+        int N = item;
+        int col_B = item;
 
-    float *A_d, *B_d, *C_d;
-    size_t bytes_A = A_h.size() * sizeof(float);
-    size_t bytes_B = B_h.size() * sizeof(float);
-    size_t bytes_C = C_h.size() * sizeof(float);
+        std::vector<float> A_h(row_A * N);
+        std::vector<float> B_h(N * col_B);
+        std::vector<float> C_h(row_A * col_B, 0.0f);
+        float cublasPer = 0.0f;
 
-    cudaMalloc((void**)&A_d, bytes_A);
-    cudaMalloc((void**)&B_d, bytes_B);
-    cudaMalloc((void**)&C_d, bytes_C); 
+        printf("Loading matrices from disk...\n");
+        loadBinaryWeights("matrix_A.bin", A_h);
+        loadBinaryWeights("matrix_B.bin", B_h);
+        printf("Matrices loaded successfully.\n");
 
-    cudaMemcpy(A_d, A_h.data(), bytes_A, cudaMemcpyHostToDevice);
-    cudaMemcpy(B_d, B_h.data(), bytes_B, cudaMemcpyHostToDevice);
+        float *A_d, *B_d, *C_d;
+        size_t bytes_A = A_h.size() * sizeof(float);
+        size_t bytes_B = B_h.size() * sizeof(float);
+        size_t bytes_C = C_h.size() * sizeof(float);
 
-    printf("\n");
-    printf("\n");
-    cudaMemset(C_d, 0, bytes_C);
-    cublasRef(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
-    printf("\n");
-    printf("\n");
+        cudaMalloc((void**)&A_d, bytes_A);
+        cudaMalloc((void**)&B_d, bytes_B);
+        cudaMalloc((void**)&C_d, bytes_C); 
 
-    
-    cudaMemset(C_d, 0, bytes_C);
-    benchMatrixMulNaive(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
-    printf("\n");
-    printf("\n");
+        cudaMemcpy(A_d, A_h.data(), bytes_A, cudaMemcpyHostToDevice);
+        cudaMemcpy(B_d, B_h.data(), bytes_B, cudaMemcpyHostToDevice);
 
-    cudaMemset(C_d, 0, bytes_C);
-    benchMatrixMulTiled(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
-    printf("\n");
-    printf("\n");
+        printf("\n");
+        printf("\n");
+        cudaMemset(C_d, 0, bytes_C);
+        cublasRef(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        printf("\n");
+        printf("\n");
 
-    cudaMemset(C_d, 0, bytes_C);
-    benchMatrixMulCoarsed_1D(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
-    printf("\n");
-    printf("\n");
+        
+        cudaMemset(C_d, 0, bytes_C);
+        benchMatrixMulNaive(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        printf("\n");
+        printf("\n");
 
-    cudaMemset(C_d, 0, bytes_C);
-    benchMatrixMulCoarsed_2D(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
-    printf("\n");
-    printf("\n");
+        cudaMemset(C_d, 0, bytes_C);
+        benchMatrixMulTiled(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        printf("\n");
+        printf("\n");
 
-    cudaMemset(C_d, 0, bytes_C);
-    benchMatrixMulWarpTiled(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
-    printf("\n");
-    printf("\n");
+        cudaMemset(C_d, 0, bytes_C);
+        benchMatrixMulCoarsed_1D(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        printf("\n");
+        printf("\n");
 
-    cudaMemset(C_d, 0, bytes_C);
-    benchMatrixMulVectorizedLoads(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
-    cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
-    verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        cudaMemset(C_d, 0, bytes_C);
+        benchMatrixMulCoarsed_2D(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        printf("\n");
+        printf("\n");
 
-    cudaFree(A_d);
-    cudaFree(B_d);
-    cudaFree(C_d);
+        cudaMemset(C_d, 0, bytes_C);
+        benchMatrixMulWarpTiled(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+        printf("\n");
+        printf("\n");
+
+        cudaMemset(C_d, 0, bytes_C);
+        benchMatrixMulVectorizedLoads(A_d, B_d, C_d, row_A, N, col_B, cublasPer);
+        cudaMemcpy(C_h.data(), C_d, bytes_C, cudaMemcpyDeviceToHost);
+        verifyCPU(C_h, "matrix_C_ref.bin", row_A, col_B, N);
+
+        cudaFree(A_d);
+        cudaFree(B_d);
+        cudaFree(C_d);
+
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
 }
